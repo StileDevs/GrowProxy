62ff0d8dbff758a5d3a27e3d82508673b3a11fe8
Hendra Gunawan <48410066+JadlionHD@users.noreply.github.com>
2023-07-07 23:12:41 +0700

    testing gtps (#2)

--- src/structures/Server.ts
+++ src/structures/Server.ts
@@ -16,6 +16,7 @@ import ansi from "ansi-colors";
 import { ProxyConfig } from "../types";
 import { parseTextToObj, parseText } from "./Utils";
 import log from "log4js";
+import crypto from "crypto";
 
 export class Server {
   public client: Client;
@@ -37,11 +38,11 @@ export class Server {
       https: {
         port,
         enable: true,
-        ip,
-        type2: true
+        ip
+        // type2: true
       }
     });
-    this.client.toggleNewPacket();
+    // this.client.toggleNewPacket();
 
     this.config = JSON.parse(readFileSync("./config.json", "utf8"));
     this.proxyNetID = 0;
@@ -74,7 +75,8 @@ export class Server {
         this.proxy.setServerNetID(netID);
         const req = await this.request();
         console.log(req);
-        this.setMeta(req.meta as string);
+
+        this.setMeta((req.meta as string).replace(/(\r)/gm, ""));
         if (
           this.proxy.client.connect(
             (req.server as string).replace(/(\r)/gm, ""),
@@ -87,6 +89,7 @@ export class Server {
           log.getLogger("CONNECT").info(`Successfully proxy connect to ${req.server}`);
       })
       .on("raw", (netID, data) => {
+        console.log(`[${netID}] Server Received`, this.toFullBuffer(data), "\n");
         const type = data.readUInt32LE(0);
         const peerProxy = new Peer(this.proxy.client, this.proxyNetID);
         const peer = new Peer(this.client, this.proxy.serverNetID);
@@ -95,7 +98,7 @@ export class Server {
           case PacketTypes.ACTION: {
             const parsed = parseTextToObj(data);
 
-            if (parsed.action === "quit") {
+            if ((parsed.action as string).replace(/\x00/gm, "") === "quit") {
               this.client._client.disconnect(netID);
             }
 
@@ -111,8 +114,16 @@ export class Server {
             let strObj = parseTextToObj(str);
 
             if (strObj["requestedName"]) {
+              const klv = this.generate_klv(
+                parseInt(strObj.protocol as string),
+                strObj.game_version as string,
+                strObj.rid as string
+              );
+              // console.log("klv", klv);
+
               strObj.meta = this.meta;
               strObj.country = "jp";
+              // strObj.klv = klv;
             }
 
             const buf = Buffer.alloc(4 + str.length);
@@ -141,7 +152,7 @@ export class Server {
               }
 
               case TankTypes.DISCONNECT: {
-                // peerProxy.send(data);
+                peerProxy.send(data);
                 break;
               }
 
@@ -168,6 +179,7 @@ export class Server {
       })
       .on("disconnect", (netID) => {
         console.log("Client disconnected", netID);
+        this.proxy.client._client.disconnect(this.proxyNetID);
       })
       .listen();
   }
@@ -188,5 +200,44 @@ export class Server {
 
     if (res.status !== 200) return null;
     return parseTextToObj(res.data);
+  }
+
+  public generate_klv(protocol: number, version: string, rid: string) {
+    const salts = [
+      "e9fc40ec08f9ea6393f59c65e37f750aacddf68490c4f92d0d2523a5bc02ea63",
+      "c85df9056ee603b849a93e1ebab5dd5f66e1fb8b2f4a8caef8d13b9f9e013fa4",
+      "3ca373dffbf463bb337e0fd768a2f395b8e417475438916506c721551f32038d",
+      "73eff5914c61a20a71ada81a6fc7780700fb1c0285659b4899bc172a24c14fc1"
+    ];
+
+    const values = [
+      crypto
+        .createHash("sha256")
+        .update(
+          crypto
+            .createHash("md5")
+            .update(crypto.createHash("sha256").update(`${protocol}`).digest("hex"))
+            .digest("hex")
+        )
+        .digest("hex"),
+      crypto
+        .createHash("sha256")
+        .update(crypto.createHash("sha256").update(version).digest("hex"))
+        .digest("hex"),
+      crypto
+        .createHash("sha256")
+        .update(crypto.createHash("sha256").update(`${protocol}${salts[3]}`).digest("hex"))
+        .digest("hex")
+    ];
+
+    const generate_rid = crypto
+      .createHash("sha256")
+      .update(crypto.createHash("md5").update(rid).digest("hex"))
+      .digest("hex");
+
+    return crypto
+      .createHash("sha256")
+      .update(values[0] + salts[0] + values[1] + salts[1] + generate_rid + salts[2] + values[2])
+      .digest("hex");
   }
 }
